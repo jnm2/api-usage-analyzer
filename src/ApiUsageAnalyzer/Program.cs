39d5abccb07fdb1b2da47df5c2571f82148d3e2a
jnm2 <me@jnm2.com>
2026-09-01 17:25:26 -0400

    Update Spectre libraries

--- src/ApiUsageAnalyzer/Program.cs
+++ src/ApiUsageAnalyzer/Program.cs
@@ -1,4 +1,7 @@
 using ApiUsageAnalyzer;
+using ApiUsageAnalyzer.Utils;
 using Spectre.Console.Cli;
 
-return await new CommandApp<CliCommand>().RunAsync(args);
+using var cancellation = ConsoleUtils.HandleCtrlC(out var cancellationToken);
+
+return await new CommandApp<CliCommand>().RunAsync(args, cancellationToken);
